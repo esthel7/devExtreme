e3ff7dadd86d3a7893472c08d915b6d8040f7e6c
esthel <esthel9026@naver.com>
2025-06-20 16:24:09 +0900

    feat: inventory drag and drop
    엑셀 열 목록들을 드래그 앤 드롭으로 view 유무 지정

--- src/app/grid/Grid.tsx
+++ src/app/grid/Grid.tsx
@@ -8,12 +8,16 @@ import DataGrid, {
   Paging
 } from 'devextreme-react/data-grid';
 import 'devextreme/dist/css/dx.light.css';
-import { useEffect, useState, useRef } from 'react';
+import { DragEvent, useEffect, useState, useRef } from 'react';
 import * as XLSX from 'xlsx';
+import styles from '@/app/page.module.css';
 
 export default function Grid() {
   const inventory = useRef<Record<string, number>>({});
   const [selectedInventory, setSelectedInventory] = useState<
+    Record<string, number>
+  >({});
+  const [remainInventory, setRemainInventory] = useState<
     Record<string, number>
   >({});
   const [excel, setExcel] = useState<(string | number)[][]>([]);
@@ -61,11 +65,95 @@ export default function Grid() {
     );
   }, [excel, selectedInventory]);
 
+  function InventoryBox() {
+    const onDrop = (
+      e: DragEvent<HTMLDivElement>,
+      to: 'unselected' | 'selected'
+    ) => {
+      const item = e.dataTransfer.getData('item');
+      const from = e.dataTransfer.getData('from');
+
+      if (from === to) return; // move to same area
+
+      if (from === 'unselected') {
+        setRemainInventory(prev => {
+          const newRemainInventory = { ...prev };
+          delete newRemainInventory[item];
+          return newRemainInventory;
+        });
+        setSelectedInventory(prev => ({
+          ...prev,
+          [item]: inventory.current[item]
+        }));
+      } else {
+        setRemainInventory(prev => ({
+          ...prev,
+          [item]: inventory.current[item]
+        }));
+        setSelectedInventory(prev => {
+          const newSelectedInventory = { ...prev };
+          delete newSelectedInventory[item];
+          return newSelectedInventory;
+        });
+      }
+    };
+
+    const onDragOver = (e: DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+    };
+
+    const onDragStart = (
+      e: DragEvent<HTMLDivElement>,
+      item: string,
+      from: 'unselected' | 'selected'
+    ) => {
+      e.dataTransfer.setData('item', item);
+      e.dataTransfer.setData('from', from);
+    };
+
+    return (
+      <div className={styles.inventoryBox}>
+        <div
+          className={styles.inventorySection}
+          draggable
+          onDrop={e => onDrop(e, 'unselected')}
+          onDragOver={onDragOver}
+        >
+          <h2>unSelected</h2>
+          {Object.keys(remainInventory).map(item => (
+            <div
+              key={item}
+              className={styles.inventoryItem}
+              draggable
+              onDragStart={e => onDragStart(e, item, 'unselected')}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div
+          className={styles.inventorySection}
+          onDrop={e => onDrop(e, 'selected')}
+          onDragOver={onDragOver}
+        >
+          <h2>selected</h2>
+          {Object.keys(selectedInventory).map(item => (
+            <div
+              key={item}
+              className={styles.inventoryItem}
+              draggable
+              onDragStart={e => onDragStart(e, item, 'selected')}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
-      {Object.keys(inventory.current).length > 0 ? (
-        <div>All Inventory: {Object.keys(inventory.current).join(' / ')}</div>
-      ) : null}
       <input
         type="file"
         accept=".xlsx, .xls"
@@ -76,6 +164,7 @@ export default function Grid() {
           marginBlock: '20px'
         }}
       />
+      {Object.keys(inventory.current).length > 0 ? <InventoryBox /> : null}
       <DataGrid
         dataSource={dataSource}
         rowAlternationEnabled={true} // Use the gray background to highlight even grid rows and improve readability
