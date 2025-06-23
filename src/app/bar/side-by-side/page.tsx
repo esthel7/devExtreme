59f31b3e71c5bce5a7544ee6d928114ddc4633b4
esthel <esthel9026@naver.com>
2025-06-23 10:51:08 +0900

    feat: devextreme side-by-side bar 적용
    데이터 순회하며 그룹화 필요
    차트 데이터 별 라벨링 위치 조절
    export 기능으로 print, png 등 가능
    chart title 달기

--- src/app/bar/side-by-side/page.tsx
+++ src/app/bar/side-by-side/page.tsx
@@ -1,7 +1,248 @@
+'use client';
+
+import {
+  Chart,
+  CommonSeriesSettings,
+  Export,
+  Format,
+  Label,
+  Legend,
+  Series,
+  ZoomAndPan
+} from 'devextreme-react/chart';
+import { DragEvent, useEffect, useRef, useState } from 'react';
+import * as XLSX from 'xlsx';
+import styles from '@/app/page.module.css';
+
 export default function Home() {
+  const inventory = useRef<Record<string, number>>({});
+  const [xInventory, setXInventory] = useState<Record<string, string>>({});
+  const [yInventory, setYInventory] = useState<Record<string, number>>({});
+  const [remainInventory, setRemainInventory] = useState<
+    Record<string, number>
+  >({});
+  const [excel, setExcel] = useState<(string | number)[][]>([]);
+  const [dataSource, setDataSource] = useState<
+    Record<string, string | number>[]
+  >([]);
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = evt => {
+      const binaryStr = evt.target?.result;
+      const workbook = XLSX.read(binaryStr, { type: 'binary' });
+
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+
+      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+      const originalInventory = jsonData.shift() as string[];
+      originalInventory.forEach((item, index) => {
+        inventory.current[item] = index;
+      });
+      setRemainInventory(inventory.current);
+      setExcel(jsonData as (string | number)[][]);
+      console.log('check excel data', jsonData);
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
+  useEffect(() => {
+    if (!Object.keys(xInventory).length || !Object.keys(yInventory).length)
+      return;
+    const xkey = Object.keys(xInventory)[0];
+    const ykeys = Object.keys(yInventory);
+    const format: Record<string, string | number>[] = [];
+    const match: Record<string, number> = {};
+    let cnt = 0;
+    excel.forEach(item => {
+      let idx = 0;
+      if (item[inventory.current[xkey]] in match)
+        idx = match[item[inventory.current[xkey]]];
+      else {
+        match[item[inventory.current[xkey]]] = cnt;
+        idx = cnt;
+        cnt++;
+        const newFormat = { [xkey]: item[inventory.current[xkey]] };
+        ykeys.forEach(item => (newFormat[item] = 0));
+        format.push(newFormat);
+      }
+      ykeys.forEach(
+        key =>
+          (format[idx][key] =
+            Number(format[idx][key]) + Number(item[inventory.current[key]]))
+      );
+    });
+    setDataSource(format);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [xInventory, yInventory]);
+
+  function InventoryBox() {
+    const onDrop = (
+      e: DragEvent<HTMLDivElement>,
+      to: 'unselected' | 'x' | 'y'
+    ) => {
+      const item = e.dataTransfer.getData('item');
+      const from = e.dataTransfer.getData('from');
+
+      if (from === to) return; // move to same area
+
+      if (from === 'unselected') {
+        if (
+          to === 'y' &&
+          typeof excel[1][inventory.current[item]] !== 'number'
+        ) {
+          alert('y값은 숫자여야 합니다.');
+          return;
+        }
+        setRemainInventory(prev => {
+          const newRemainInventory = { ...prev };
+          delete newRemainInventory[item];
+          return newRemainInventory;
+        });
+        if (to === 'x')
+          setXInventory({
+            [String(item)]: String(inventory.current[item])
+          });
+        else
+          setYInventory(prev => ({
+            ...prev,
+            [String(item)]: Number(inventory.current[item])
+          }));
+      } else {
+        setRemainInventory(prev => ({
+          ...prev,
+          [item]: inventory.current[item]
+        }));
+        if (from === 'x') setXInventory({});
+        else
+          setYInventory(prev => {
+            const newRemainInventory = { ...prev };
+            delete newRemainInventory[item];
+            return newRemainInventory;
+          });
+      }
+    };
+
+    const onDragOver = (e: DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+    };
+
+    const onDragStart = (
+      e: DragEvent<HTMLDivElement>,
+      item: string,
+      from: 'unselected' | 'x' | 'y'
+    ) => {
+      e.dataTransfer.setData('item', item);
+      e.dataTransfer.setData('from', from);
+    };
+
+    return (
+      <div className={styles.inventoryBox}>
+        <div
+          className={styles.inventorySection}
+          draggable
+          onDrop={e => onDrop(e, 'unselected')}
+          onDragOver={onDragOver}
+        >
+          <h2>unSelected</h2>
+          {Object.keys(remainInventory).map(item => (
+            <div
+              key={item}
+              className={styles.inventoryItem}
+              draggable
+              onDragStart={e => onDragStart(e, item, 'unselected')}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'x')}
+            onDragOver={onDragOver}
+          >
+            <h2>x</h2>
+            {Object.keys(xInventory).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'x')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'y')}
+            onDragOver={onDragOver}
+          >
+            <h2>y</h2>
+            {Object.keys(yInventory).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'y')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Side By Side</h1>
-    </div>
+    <>
+      <input
+        type="file"
+        accept=".xlsx, .xls"
+        onChange={handleFileUpload}
+        style={{
+          height: '100px',
+          border: '1px solid blue',
+          marginBlock: '20px'
+        }}
+      />
+      {Object.keys(inventory.current).length > 0 ? <InventoryBox /> : null}
+      <Chart id="chart" title="side by side bar" dataSource={dataSource}>
+        <CommonSeriesSettings
+          argumentField={Object.keys(xInventory)[0]}
+          type="bar"
+          hoverMode="allArgumentPoints"
+          selectionMode="allArgumentPoints"
+        >
+          {/* individual values in chart */}
+          <Label visible={true}>
+            <Format type="fixedPoint" precision={0} />
+          </Label>
+        </CommonSeriesSettings>
+
+        {Object.keys(yInventory).map(item => (
+          <Series
+            key={item}
+            valueField={item} // y value
+            argumentField={Object.keys(xInventory)[0]} // x value
+            name={item}
+          />
+        ))}
+
+        {/* location of chart property */}
+        <Legend verticalAlignment="bottom" horizontalAlignment="center" />
+
+        <ZoomAndPan argumentAxis="both" valueAxis="both" />
+        <Export enabled={true} />
+      </Chart>
+    </>
   );
 }
