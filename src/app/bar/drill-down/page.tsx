0cccdc6aafd55c13a4a6d5136615ddb2f1d54aea
esthel <esthel9026@naver.com>
2025-06-23 14:39:41 +0900

    feat: devextreme drill-down bar 적용
    데이터 형식이 {arg: xx, val: xx, parentId: xx}로 정해져있음
    초기 데이터의 경우 arg값이 나오고 클릭할 경우 paretnId가 동일한 값만 나옴

--- src/app/bar/drill-down/page.tsx
+++ src/app/bar/drill-down/page.tsx
@@ -1,7 +1,278 @@
+'use client';
+
+import {
+  Chart,
+  Series,
+  Legend,
+  ValueAxis,
+  ZoomAndPan
+} from 'devextreme-react/chart';
+import { DragEvent, useEffect, useRef, useState } from 'react';
+import * as XLSX from 'xlsx';
+import styles from '@/app/page.module.css';
+import dstyles from './drill-down.module.css';
+
 export default function Home() {
+  const inventory = useRef<Record<string, number>>({});
+  const [xInventory, setXInventory] = useState<Record<string, string>>({});
+  const [yInventory, setYInventory] = useState<Record<string, number>>({});
+  const [remainInventory, setRemainInventory] = useState<
+    Record<string, number>
+  >({});
+  const [excel, setExcel] = useState<(string | number)[][]>([]);
+  const [dataSource, setDataSource] = useState<
+    Record<string, string | number>[]
+  >([]);
+  const [isFirstLevel, setIsFirstLevel] = useState(true);
+  const [area, setArea] = useState('');
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = evt => {
+      const binaryStr = evt.target?.result;
+      const workbook = XLSX.read(binaryStr, { type: 'binary' });
+
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+
+      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+      const originalInventory = jsonData.shift() as string[];
+      originalInventory.forEach((item, index) => {
+        inventory.current[item] = index;
+      });
+      setRemainInventory(inventory.current);
+      setExcel(jsonData as (string | number)[][]);
+      console.log('check excel data', jsonData);
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
+  function InventoryBox() {
+    const onDrop = (
+      e: DragEvent<HTMLDivElement>,
+      to: 'unselected' | 'x' | 'y'
+    ) => {
+      const item = e.dataTransfer.getData('item');
+      const from = e.dataTransfer.getData('from');
+
+      if (from === to) return; // move to same area
+
+      if (from === 'unselected') {
+        if (
+          to === 'y' &&
+          typeof excel[1][inventory.current[item]] !== 'number'
+        ) {
+          alert('y값은 숫자여야 합니다.');
+          return;
+        }
+        if (to === 'x' && item !== '시도') {
+          alert('drill-down 그래프에는 "시도"만 가능합니다.');
+          return;
+        }
+        setRemainInventory(prev => {
+          const newRemainInventory = { ...prev };
+          delete newRemainInventory[item];
+          return newRemainInventory;
+        });
+        if (to === 'x')
+          setXInventory({
+            [String(item)]: String(inventory.current[item])
+          });
+        else
+          setYInventory({
+            [String(item)]: Number(inventory.current[item])
+          });
+      } else {
+        setRemainInventory(prev => ({
+          ...prev,
+          [item]: inventory.current[item]
+        }));
+        if (from === 'x') setXInventory({});
+        else setYInventory({});
+      }
+    };
+
+    const onDragOver = (e: DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+    };
+
+    const onDragStart = (
+      e: DragEvent<HTMLDivElement>,
+      item: string,
+      from: 'unselected' | 'x' | 'y'
+    ) => {
+      e.dataTransfer.setData('item', item);
+      e.dataTransfer.setData('from', from);
+    };
+
+    return (
+      <div className={styles.inventoryBox}>
+        <div
+          className={styles.inventorySection}
+          draggable
+          onDrop={e => onDrop(e, 'unselected')}
+          onDragOver={onDragOver}
+        >
+          <h2>unSelected</h2>
+          {Object.keys(remainInventory).map(item => (
+            <div
+              key={item}
+              className={styles.inventoryItem}
+              draggable
+              onDragStart={e => onDragStart(e, item, 'unselected')}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'x')}
+            onDragOver={onDragOver}
+          >
+            <h2>x</h2>
+            {Object.keys(xInventory).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'x')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'y')}
+            onDragOver={onDragOver}
+          >
+            <h2>y</h2>
+            {Object.keys(yInventory).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'y')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function onPointClick(e: any) {
+    if (isFirstLevel) {
+      setIsFirstLevel(false);
+      setArea(e.target.originalArgument.toString());
+    }
+  }
+
+  function customizePoint(): {
+    color: string;
+    hoverStyle?: {
+      color?: string;
+      hatching?: string;
+    };
+  } {
+    const colors = ['#6babac', '#e55253'];
+    return {
+      color: colors[Number(isFirstLevel)],
+      hoverStyle: !isFirstLevel
+        ? {
+            hatching: 'none'
+          }
+        : {}
+    };
+  }
+
+  function onButtonClick() {
+    if (!isFirstLevel) setIsFirstLevel(true);
+  }
+
+  useEffect(() => {
+    if (!Object.keys(xInventory).length || !Object.keys(yInventory).length)
+      return;
+    const xkey = Object.keys(xInventory)[0];
+    const ykey = Object.keys(yInventory)[0];
+    const format: Record<string, string | number>[] = [];
+    const match: Record<string, number> = {};
+    let cnt = 0;
+    excel.forEach(item => {
+      if (!isFirstLevel && item[inventory.current[xkey]] !== area) return;
+      const filterXkey = isFirstLevel ? '시도' : '시군구';
+      let idx = 0;
+      if (item[inventory.current[filterXkey]] in match)
+        idx = match[item[inventory.current[filterXkey]]];
+      else {
+        match[item[inventory.current[filterXkey]]] = cnt;
+        idx = cnt;
+        cnt++;
+        const newFormat = {
+          arg: item[inventory.current[filterXkey]],
+          val: 0,
+          parentId: item[inventory.current['시도']]
+        };
+        format.push(newFormat);
+      }
+      format[idx]['val'] =
+        Number(format[idx]['val']) + Number(item[inventory.current[ykey]]);
+    });
+    console.log('check graph data', format);
+    setDataSource(format);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isFirstLevel, xInventory, yInventory]);
+
   return (
-    <div>
-      <h1>Drill Down</h1>
-    </div>
+    <>
+      <input
+        type="file"
+        accept=".xlsx, .xls"
+        onChange={handleFileUpload}
+        style={{
+          height: '100px',
+          border: '1px solid blue',
+          marginBlock: '20px'
+        }}
+      />
+      {Object.keys(inventory.current).length > 0 ? <InventoryBox /> : null}
+      <div className={dstyles.chartBox}>
+        <Chart
+          id="chart"
+          title="drill-down bar"
+          dataSource={dataSource}
+          // control graph view
+          customizePoint={customizePoint}
+          // start data filtering
+          onPointClick={onPointClick}
+          className={isFirstLevel ? 'pointer-on-bars' : ''}
+        >
+          <Series type="bar" />
+
+          {/* chart value property */}
+          <ValueAxis showZero={false} />
+
+          {/* location of chart property */}
+          <Legend visible={false} />
+
+          <ZoomAndPan argumentAxis="both" valueAxis="both" />
+        </Chart>
+        {isFirstLevel ? null : (
+          <button className={dstyles.backButton} onClick={onButtonClick}>
+            Back
+          </button>
+        )}
+      </div>
+    </>
   );
 }
