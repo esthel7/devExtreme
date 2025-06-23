c02a920e0f07d1841b4d9ec8ab5cd94c486f7fc7
esthel <esthel9026@naver.com>
2025-06-23 09:28:48 +0900

    feat: devextreme bar 적용

--- src/app/bar/Bar.tsx
+++ src/app/bar/Bar.tsx
@@ -1,3 +1,201 @@
+'use client';
+
+import { DragEvent, useState, useEffect, useRef } from 'react';
+import { Chart, Series, ZoomAndPan } from 'devextreme-react/chart';
+import * as XLSX from 'xlsx';
+import styles from '@/app/page.module.css';
+
 export default function Bar() {
-  return <div>bar</div>;
+  const inventory = useRef<Record<string, number>>({});
+  const [xInventory, setXInventory] = useState<Record<string, string>>({});
+  const [yInventory, setYInventory] = useState<Record<string, number>>({});
+  const [remainInventory, setRemainInventory] = useState<
+    Record<string, number>
+  >({});
+  const [excel, setExcel] = useState<(string | number)[][]>([]);
+  const [dataSource, setDataSource] = useState<
+    Record<string, string | number>[]
+  >([]);
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = evt => {
+      const binaryStr = evt.target?.result;
+      const workbook = XLSX.read(binaryStr, { type: 'binary' });
+
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+
+      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+      const originalInventory = jsonData.shift() as string[];
+      originalInventory.forEach((item, index) => {
+        inventory.current[item] = index;
+      });
+      setRemainInventory(inventory.current);
+      setExcel(jsonData as (string | number)[][]);
+      console.log('check excel data', jsonData);
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
+  useEffect(() => {
+    if (!Object.keys(xInventory).length || !Object.keys(yInventory).length)
+      return;
+    const xkey = Object.keys(xInventory)[0];
+    const ykey = Object.keys(yInventory)[0];
+    setDataSource(
+      excel.map(item => {
+        return {
+          [xkey]: item[inventory.current[xkey]],
+          [ykey]: item[inventory.current[ykey]]
+        };
+      })
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [xInventory, yInventory]);
+
+  function InventoryBox() {
+    const onDrop = (
+      e: DragEvent<HTMLDivElement>,
+      to: 'unselected' | 'x' | 'y'
+    ) => {
+      const item = e.dataTransfer.getData('item');
+      const from = e.dataTransfer.getData('from');
+
+      if (from === to) return; // move to same area
+
+      if (from === 'unselected') {
+        if (
+          to === 'y' &&
+          typeof excel[1][inventory.current[item]] !== 'number'
+        ) {
+          alert('y값은 숫자여야 합니다.');
+          return;
+        }
+        setRemainInventory(prev => {
+          const newRemainInventory = { ...prev };
+          delete newRemainInventory[item];
+          return newRemainInventory;
+        });
+        if (to === 'x')
+          setXInventory({
+            [String(item)]: String(inventory.current[item])
+          });
+        else
+          setYInventory({
+            [String(item)]: Number(inventory.current[item])
+          });
+      } else {
+        setRemainInventory(prev => ({
+          ...prev,
+          [item]: inventory.current[item]
+        }));
+        if (from === 'x') setXInventory({});
+        else setYInventory({});
+      }
+    };
+
+    const onDragOver = (e: DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+    };
+
+    const onDragStart = (
+      e: DragEvent<HTMLDivElement>,
+      item: string,
+      from: 'unselected' | 'x' | 'y'
+    ) => {
+      e.dataTransfer.setData('item', item);
+      e.dataTransfer.setData('from', from);
+    };
+
+    return (
+      <div className={styles.inventoryBox}>
+        <div
+          className={styles.inventorySection}
+          draggable
+          onDrop={e => onDrop(e, 'unselected')}
+          onDragOver={onDragOver}
+        >
+          <h2>unSelected</h2>
+          {Object.keys(remainInventory).map(item => (
+            <div
+              key={item}
+              className={styles.inventoryItem}
+              draggable
+              onDragStart={e => onDragStart(e, item, 'unselected')}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'x')}
+            onDragOver={onDragOver}
+          >
+            <h2>x</h2>
+            {Object.keys(xInventory).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'x')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'y')}
+            onDragOver={onDragOver}
+          >
+            <h2>y</h2>
+            {Object.keys(yInventory).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'y')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <input
+        type="file"
+        accept=".xlsx, .xls"
+        onChange={handleFileUpload}
+        style={{
+          height: '100px',
+          border: '1px solid blue',
+          marginBlock: '20px'
+        }}
+      />
+      {Object.keys(inventory.current).length > 0 ? <InventoryBox /> : null}
+      <Chart id="chart" dataSource={dataSource}>
+        <Series
+          valueField={Object.keys(yInventory)[0]} // y value
+          argumentField={Object.keys(xInventory)[0]} // x value
+          name={Object.keys(xInventory)[0]}
+          type="bar"
+          color="#ffaa66"
+        />
+        <ZoomAndPan argumentAxis="both" valueAxis="both" />
+      </Chart>
+    </>
+  );
 }
