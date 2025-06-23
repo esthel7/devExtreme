c8d7bb912d13fd0351bd1b2abb1122e09fd9cad6
esthel <esthel9026@naver.com>
2025-06-23 09:39:56 +0900

    feat: bar layout 처리

---
+++ src/app/bar/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Create Next App',
+  description: 'Bar chart from devExtreme'
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <br />
+      <h5>Bar Section</h5>
+      <Link href="/bar">Standard Bar</Link>
+      <br />
+      <Link href="/bar/side-by-side">Side By Side Bar</Link>
+      <br />
+      {children}
+    </>
+  );
+}
---
+++ src/app/bar/side-by-side/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Side By Side</h1>
+    </div>
+  );
+}
