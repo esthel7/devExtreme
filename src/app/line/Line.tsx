d27dd58841fda39a829ac18178f3437d35178081
esthel <esthel9026@naver.com>
2025-06-24 12:00:23 +0900

    feat: devextreme line 적용

--- src/app/line/Line.tsx
+++ src/app/line/Line.tsx
@@ -1,7 +1,297 @@
+'use client';
+
+import { ChangeEvent, DragEvent, useEffect, useRef, useState } from 'react';
+import {
+  Chart,
+  Series,
+  ArgumentAxis,
+  CommonSeriesSettings,
+  Export,
+  Legend,
+  Margin,
+  Title,
+  Subtitle,
+  Tooltip,
+  Grid
+} from 'devextreme-react/chart';
+import * as XLSX from 'xlsx';
+import styles from '@/app/page.module.css';
+import lstyles from './line.module.css';
+
+type ChartType = 'line' | 'stackedline' | 'fullstackedline';
+
 export default function Line() {
+  const inventory = useRef<Record<string, number>>({});
+  const [xInventory, setXInventory] = useState<Record<string, string>>({});
+  const [yInventory, setYInventory] = useState<Record<string, number>>({});
+  const [remainInventory, setRemainInventory] = useState<
+    Record<string, number>
+  >({});
+  const [excel, setExcel] = useState<(string | number)[][]>([]);
+  const [dataSource, setDataSource] = useState<
+    Record<string, string | number>[]
+  >([]);
+  const [type, setType] = useState<ChartType>('line');
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = evt => {
+      const binaryStr = evt.target?.result;
+      const workbook = XLSX.read(binaryStr, { type: 'binary' });
+
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+
+      inventory.current = {};
+      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+      const originalInventory = jsonData.shift() as string[];
+      originalInventory.forEach((item, index) => {
+        inventory.current[item] = index;
+      });
+      setRemainInventory(inventory.current);
+      setExcel(jsonData as (string | number)[][]);
+      console.log('check excel data', jsonData);
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
+  useEffect(() => {
+    if (!Object.keys(xInventory).length || !Object.keys(yInventory).length) {
+      setDataSource([]);
+      return;
+    }
+    const xkey = Object.keys(xInventory)[0];
+    const ykeys = Object.keys(yInventory);
+    const format: Record<string, string | number>[] = [];
+    const match: Record<string, number> = {};
+    let cnt = 0;
+    excel.forEach(item => {
+      let idx = 0;
+      if (item[inventory.current[xkey]] in match)
+        idx = match[item[inventory.current[xkey]]];
+      else {
+        match[item[inventory.current[xkey]]] = cnt;
+        idx = cnt;
+        cnt++;
+        const newFormat = { [xkey]: item[inventory.current[xkey]] };
+        ykeys.forEach(item => (newFormat[item] = 0));
+        format.push(newFormat);
+      }
+      ykeys.forEach(
+        key =>
+          (format[idx][key] =
+            Number(format[idx][key]) + Number(item[inventory.current[key]]))
+      );
+    });
+    console.log(format);
+    setDataSource(format);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [xInventory, yInventory]);
+
+  function InventoryBox() {
+    const onDrop = (
+      e: DragEvent<HTMLDivElement>,
+      to: 'unselected' | 'x' | 'y'
+    ) => {
+      const item = e.dataTransfer.getData('item');
+      const from = e.dataTransfer.getData('from');
+
+      if (from === to) return; // move to same area
+      if (to === 'y' && typeof excel[0][inventory.current[item]] !== 'number') {
+        alert('y값은 숫자여야 합니다.');
+        return;
+      }
+
+      switch (to) {
+        case 'unselected':
+          setRemainInventory(prev => ({
+            ...prev,
+            [item]: inventory.current[item]
+          }));
+          break;
+        case 'x':
+          if (Object.keys(xInventory).length)
+            setRemainInventory(prev => ({
+              ...prev,
+              [Object.keys(xInventory)[0]]:
+                inventory.current[Object.keys(xInventory)[0]]
+            }));
+          setXInventory({
+            [String(item)]: String(inventory.current[item])
+          });
+          break;
+        case 'y':
+          setYInventory(prev => ({
+            ...prev,
+            [item]: inventory.current[item]
+          }));
+          break;
+        default:
+          console.error('error');
+          break;
+      }
+
+      switch (from) {
+        case 'unselected':
+          setRemainInventory(prev => {
+            const newRemainInventory = { ...prev };
+            delete newRemainInventory[item];
+            return newRemainInventory;
+          });
+          break;
+        case 'x':
+          setXInventory({});
+          break;
+        case 'y':
+          setYInventory(prev => {
+            const newRemainInventory = { ...prev };
+            delete newRemainInventory[item];
+            return newRemainInventory;
+          });
+          break;
+        default:
+          console.error('error');
+          break;
+      }
+    };
+
+    const onDragOver = (e: DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+    };
+
+    const onDragStart = (
+      e: DragEvent<HTMLDivElement>,
+      item: string,
+      from: 'unselected' | 'x' | 'y'
+    ) => {
+      e.dataTransfer.setData('item', item);
+      e.dataTransfer.setData('from', from);
+    };
+
+    return (
+      <div className={styles.inventoryBox}>
+        <div
+          className={styles.inventorySection}
+          onDrop={e => onDrop(e, 'unselected')}
+          onDragOver={onDragOver}
+        >
+          <h2>unSelected</h2>
+          {Object.keys(remainInventory).map(item => (
+            <div
+              key={item}
+              className={styles.inventoryItem}
+              draggable
+              onDragStart={e => onDragStart(e, item, 'unselected')}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'x')}
+            onDragOver={onDragOver}
+          >
+            <h2>x</h2>
+            {Object.keys(xInventory).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'x')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'y')}
+            onDragOver={onDragOver}
+          >
+            <h2>y</h2>
+            {Object.keys(yInventory).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'y')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  function handleChange(e: ChangeEvent<HTMLSelectElement>) {
+    setType(e.target.value as ChartType);
+  }
+
   return (
-    <div>
-      <h1>line</h1>
-    </div>
+    <>
+      <input
+        type="file"
+        accept=".xlsx, .xls"
+        onChange={handleFileUpload}
+        style={{
+          height: '100px',
+          border: '1px solid blue',
+          marginBlock: '20px'
+        }}
+      />
+      {Object.keys(inventory.current).length > 0 ? <InventoryBox /> : null}
+      <Chart palette="Violet" dataSource={dataSource}>
+        <CommonSeriesSettings
+          argumentField={Object.keys(xInventory)[0]}
+          type={type}
+        />
+        {Object.keys(yInventory).map(item => (
+          <Series key={item} valueField={item} name={item} />
+        ))}
+        <Title text="Line">
+          <Subtitle text="devExtreme" />
+        </Title>
+        <Margin bottom={20} />
+
+        {/* start with no margin */}
+        <ArgumentAxis
+          valueMarginsEnabled={false}
+          discreteAxisDivisionMode="crossLabels"
+        >
+          {/* view vertical line */}
+          <Grid visible={true} />
+        </ArgumentAxis>
+
+        {/* location of chart property */}
+        <Legend
+          verticalAlignment="bottom"
+          horizontalAlignment="center"
+          itemTextPosition="bottom"
+        />
+
+        <Export enabled={true} />
+        <Tooltip enabled={true} />
+      </Chart>
+      <select className={lstyles.optionBox} onChange={handleChange}>
+        <option className={lstyles.option} value="line">
+          line
+        </option>
+        <option className={lstyles.option} value="stackedline">
+          stackedline
+        </option>
+        <option className={lstyles.option} value="fullstackedline">
+          fullstackedline
+        </option>
+      </select>
+    </>
   );
 }
