d6bbd8007f43e48ab7b9e22b47ecf1100439aa6d
esthel <esthel9026@naver.com>
2025-06-24 10:14:15 +0900

    feat: devextreme multiple doughnut 적용

--- src/app/doughnut/multiple/page.tsx
+++ src/app/doughnut/multiple/page.tsx
@@ -1,7 +1,287 @@
+'use client';
+
+import { DragEvent, useEffect, useRef, useState } from 'react';
+import PieChart, {
+  CommonSeriesSettings,
+  Legend,
+  Series,
+  Export,
+  Label,
+  Title,
+  Tooltip,
+  Subtitle
+} from 'devextreme-react/pie-chart';
+import * as XLSX from 'xlsx';
+import styles from '@/app/page.module.css';
+
 export default function Home() {
+  const inventory = useRef<Record<string, number>>({});
+  const [category, setCategory] = useState<Record<string, string>>({});
+  const [value, setValue] = useState<Record<string, number>>({});
+  const [remainInventory, setRemainInventory] = useState<
+    Record<string, number>
+  >({});
+  const [excel, setExcel] = useState<(string | number)[][]>([]);
+  const [dataSource, setDataSource] = useState<
+    Record<string, string | number>[]
+  >([]);
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = evt => {
+      const binaryStr = evt.target?.result;
+      const workbook = XLSX.read(binaryStr, { type: 'binary' });
+
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+
+      inventory.current = {};
+      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+      const originalInventory = jsonData.shift() as string[];
+      originalInventory.forEach((item, index) => {
+        inventory.current[item] = index;
+      });
+      setRemainInventory(inventory.current);
+      setExcel(jsonData as (string | number)[][]);
+      console.log('check excel data', jsonData);
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
+  useEffect(() => {
+    if (!Object.keys(category).length || !Object.keys(value).length) {
+      setDataSource([]);
+      return;
+    }
+    const xkey = Object.keys(category)[0];
+    const ykeys = Object.keys(value);
+    const format: Record<string, string | number>[] = [];
+    const match: Record<string, number> = {};
+    let cnt = 0;
+    excel.forEach(item => {
+      let idx = 0;
+      if (item[inventory.current[xkey]] in match)
+        idx = match[item[inventory.current[xkey]]];
+      else {
+        match[item[inventory.current[xkey]]] = cnt;
+        idx = cnt;
+        cnt++;
+        const newFormat = { [xkey]: item[inventory.current[xkey]] };
+        ykeys.forEach(item => (newFormat[item] = 0));
+        format.push(newFormat);
+      }
+      ykeys.forEach(
+        key =>
+          (format[idx][key] =
+            Number(format[idx][key]) + Number(item[inventory.current[key]]))
+      );
+    });
+    console.log(format);
+    setDataSource(format);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [category, value]);
+
+  function InventoryBox() {
+    const onDrop = (
+      e: DragEvent<HTMLDivElement>,
+      to: 'unselected' | 'category' | 'value'
+    ) => {
+      const item = e.dataTransfer.getData('item');
+      const from = e.dataTransfer.getData('from');
+
+      if (from === to) return; // move to same area
+      if (
+        to === 'value' &&
+        typeof excel[0][inventory.current[item]] !== 'number'
+      ) {
+        alert('value값은 숫자여야 합니다.');
+        return;
+      }
+
+      switch (to) {
+        case 'unselected':
+          setRemainInventory(prev => ({
+            ...prev,
+            [item]: inventory.current[item]
+          }));
+          break;
+        case 'category':
+          if (Object.keys(category).length)
+            setRemainInventory(prev => ({
+              ...prev,
+              [Object.keys(category)[0]]:
+                inventory.current[Object.keys(category)[0]]
+            }));
+          setCategory({
+            [String(item)]: String(inventory.current[item])
+          });
+          break;
+        case 'value':
+          setValue(prev => ({
+            ...prev,
+            [item]: inventory.current[item]
+          }));
+          break;
+        default:
+          console.error('error');
+          break;
+      }
+
+      switch (from) {
+        case 'unselected':
+          setRemainInventory(prev => {
+            const newRemainInventory = { ...prev };
+            delete newRemainInventory[item];
+            return newRemainInventory;
+          });
+          break;
+        case 'category':
+          setCategory({});
+          break;
+        case 'value':
+          setValue(prev => {
+            const newRemainValue = { ...prev };
+            delete newRemainValue[item];
+            return newRemainValue;
+          });
+          break;
+        default:
+          console.error('error');
+          break;
+      }
+    };
+
+    const onDragOver = (e: DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+    };
+
+    const onDragStart = (
+      e: DragEvent<HTMLDivElement>,
+      item: string,
+      from: 'unselected' | 'category' | 'value'
+    ) => {
+      e.dataTransfer.setData('item', item);
+      e.dataTransfer.setData('from', from);
+    };
+
+    return (
+      <div className={styles.inventoryBox}>
+        <div
+          className={styles.inventorySection}
+          onDrop={e => onDrop(e, 'unselected')}
+          onDragOver={onDragOver}
+        >
+          <h2>unSelected</h2>
+          {Object.keys(remainInventory).map(item => (
+            <div
+              key={item}
+              className={styles.inventoryItem}
+              draggable
+              onDragStart={e => onDragStart(e, item, 'unselected')}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'category')}
+            onDragOver={onDragOver}
+          >
+            <h2>category</h2>
+            {Object.keys(category).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'category')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'value')}
+            onDragOver={onDragOver}
+          >
+            <h2>value</h2>
+            {Object.keys(value).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'value')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  function customizeTooltip(arg: {
+    argumentText: string;
+    seriesName: string;
+    valueText: string;
+  }) {
+    return {
+      text: `${arg.argumentText}<br>${arg.seriesName}: ${arg.valueText}`
+    };
+  }
+
   return (
-    <div>
-      <h1>Multiple</h1>
-    </div>
+    <>
+      <input
+        type="file"
+        accept=".xlsx, .xls"
+        onChange={handleFileUpload}
+        style={{
+          height: '100px',
+          border: '1px solid blue',
+          marginBlock: '20px'
+        }}
+      />
+      {Object.keys(inventory.current).length > 0 ? <InventoryBox /> : null}
+      <PieChart
+        id="pie"
+        type="doughnut"
+        innerRadius={0.2}
+        palette="Ocean"
+        dataSource={dataSource}
+      >
+        <Title text="Multiple Doughnut">
+          <Subtitle text="devExtreme" />
+        </Title>
+
+        <CommonSeriesSettings>
+          <Label visible={false} />
+        </CommonSeriesSettings>
+
+        {Object.keys(value).map(item => (
+          <Series
+            key={item}
+            name={item}
+            argumentField={Object.keys(category)[0]}
+            valueField={item}
+          />
+        ))}
+
+        <Export enabled={true} />
+
+        {/* location of chart property */}
+        <Legend visible={true} />
+
+        <Tooltip enabled={true} customizeTooltip={customizeTooltip} />
+      </PieChart>
+    </>
   );
 }
