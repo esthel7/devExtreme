56e6cdd8403f48f5d15d6ab9f482c01fa046eda0
esthel <esthel9026@naver.com>
2025-06-25 10:11:14 +0900

    feat: devextreme candlestick financial 적용
    y범위를 매직넘버로 지정해놔서 수정 필요

--- src/app/financial/candlestick/page.tsx
+++ src/app/financial/candlestick/page.tsx
@@ -1,7 +1,415 @@
+'use client';
+
+import { DragEvent, useEffect, useRef, useState } from 'react';
+import Chart, {
+  CommonSeriesSettings,
+  Series,
+  Reduction,
+  ArgumentAxis,
+  Label,
+  Format,
+  ValueAxis,
+  Title,
+  Legend,
+  Export,
+  Tooltip,
+  ZoomAndPan
+} from 'devextreme-react/chart';
+import { handleFileUpload } from '@/utils/handleFileUpload';
+import styles from '@/app/page.module.css';
+
 export default function Home() {
+  const inventory = useRef<Record<string, number>>({});
+  const [xInventory, setXInventory] = useState<Record<string, Date>>({});
+  const [open, setOpen] = useState<Record<string, number>>({});
+  const [close, setClose] = useState<Record<string, number>>({});
+  const [high, setHigh] = useState<Record<string, number>>({});
+  const [low, setLow] = useState<Record<string, number>>({});
+  const [remainInventory, setRemainInventory] = useState<
+    Record<string, number>
+  >({});
+  const [excel, setExcel] = useState<(string | number)[][]>([]);
+  const [dataSource, setDataSource] = useState<
+    Record<string, string | number | Date>[]
+  >([]);
+
+  useEffect(() => {
+    if (
+      !Object.keys(xInventory).length ||
+      !Object.keys(open).length ||
+      !Object.keys(close).length ||
+      !Object.keys(high).length ||
+      !Object.keys(low).length
+    ) {
+      setDataSource([]);
+      return;
+    }
+    const xkey = Object.keys(xInventory)[0];
+    const openItem = Object.keys(open)[0];
+    const closeItem = Object.keys(close)[0];
+    const highItem = Object.keys(high)[0];
+    const lowItem = Object.keys(low)[0];
+    const format: Record<string, string | number | Date>[] = [];
+    const match: Record<string, number> = {};
+    let cnt = 0;
+
+    // 형식에 맞지 않는 엑셀일때는 기존 값(open, close, high, low)에 더함
+    // 디버그 필요
+    excel.forEach(item => {
+      let idx = 0;
+      if (item[inventory.current[xkey]] in match)
+        idx = match[item[inventory.current[xkey]]];
+      else {
+        match[item[inventory.current[xkey]]] = cnt;
+        idx = cnt;
+        cnt++;
+        format.push({
+          [xkey]: new Date(item[inventory.current[xkey]]),
+          o: 0,
+          c: 0,
+          h: 0,
+          l: 0
+        });
+      }
+      format[idx]['o'] =
+        Number(format[idx]['o']) + Number(item[inventory.current[openItem]]);
+      format[idx]['c'] =
+        Number(format[idx]['c']) + Number(item[inventory.current[closeItem]]);
+      format[idx]['h'] =
+        Number(format[idx]['h']) + Number(item[inventory.current[highItem]]);
+      format[idx]['l'] =
+        Number(format[idx]['l']) + Number(item[inventory.current[lowItem]]);
+    });
+    console.log(format);
+    setDataSource(format);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [xInventory, open, close, high, low]);
+
+  function InventoryBox() {
+    const onDrop = (
+      e: DragEvent<HTMLDivElement>,
+      to: 'unselected' | 'x' | 'open' | 'close' | 'high' | 'low'
+    ) => {
+      const item = e.dataTransfer.getData('item');
+      const from = e.dataTransfer.getData('from');
+
+      if (from === to) return; // move to same area
+      if (
+        to === 'x' &&
+        !(
+          typeof excel[0][inventory.current[item]] === 'string' &&
+          /^\d{1,2}\/\d{1,2}\/\d{2}$/.test(
+            String(excel[0][inventory.current[item]])
+          )
+        )
+      ) {
+        alert('x값은 날짜여야 합니다.');
+        return;
+      }
+      if (
+        ['open', 'close', 'high', 'low'].includes(to) &&
+        typeof excel[0][inventory.current[item]] !== 'number'
+      ) {
+        console.log(
+          typeof excel[0][inventory.current[item]],
+          excel[0][inventory.current[item]]
+        );
+        alert('값은 숫자여야 합니다.');
+        return;
+      }
+
+      switch (to) {
+        case 'unselected':
+          setRemainInventory(prev => ({
+            ...prev,
+            [item]: inventory.current[item]
+          }));
+          break;
+        case 'x':
+          if (Object.keys(xInventory).length)
+            setRemainInventory(prev => ({
+              ...prev,
+              [Object.keys(xInventory)[0]]:
+                inventory.current[Object.keys(xInventory)[0]]
+            }));
+          setXInventory({
+            [item]: new Date(inventory.current[item])
+          });
+          break;
+        case 'open':
+          if (Object.keys(open).length)
+            setRemainInventory(prev => ({
+              ...prev,
+              [Object.keys(open)[0]]: inventory.current[Object.keys(open)[0]]
+            }));
+          setOpen({
+            [String(item)]: Number(inventory.current[item])
+          });
+          break;
+        case 'close':
+          if (Object.keys(close).length)
+            setRemainInventory(prev => ({
+              ...prev,
+              [Object.keys(close)[0]]: inventory.current[Object.keys(close)[0]]
+            }));
+          setClose({
+            [String(item)]: Number(inventory.current[item])
+          });
+          break;
+        case 'high':
+          if (Object.keys(high).length)
+            setRemainInventory(prev => ({
+              ...prev,
+              [Object.keys(high)[0]]: inventory.current[Object.keys(high)[0]]
+            }));
+          setHigh({
+            [String(item)]: Number(inventory.current[item])
+          });
+          break;
+        case 'low':
+          if (Object.keys(low).length)
+            setRemainInventory(prev => ({
+              ...prev,
+              [Object.keys(low)[0]]: inventory.current[Object.keys(low)[0]]
+            }));
+          setLow({
+            [String(item)]: Number(inventory.current[item])
+          });
+          break;
+        default:
+          console.error('error');
+          break;
+      }
+
+      switch (from) {
+        case 'unselected':
+          setRemainInventory(prev => {
+            const newRemainInventory = { ...prev };
+            delete newRemainInventory[item];
+            return newRemainInventory;
+          });
+          break;
+        case 'x':
+          setXInventory({});
+          break;
+        case 'open':
+          setOpen({});
+          break;
+        case 'close':
+          setClose({});
+          break;
+        case 'high':
+          setHigh({});
+          break;
+        case 'low':
+          setLow({});
+          break;
+        default:
+          console.error('error');
+          break;
+      }
+    };
+
+    const onDragOver = (e: DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+    };
+
+    const onDragStart = (
+      e: DragEvent<HTMLDivElement>,
+      item: string,
+      from: 'unselected' | 'x' | 'open' | 'close' | 'high' | 'low'
+    ) => {
+      e.dataTransfer.setData('item', item);
+      e.dataTransfer.setData('from', from);
+    };
+
+    return (
+      <div className={styles.inventoryBox}>
+        <div
+          className={styles.inventorySection}
+          onDrop={e => onDrop(e, 'unselected')}
+          onDragOver={onDragOver}
+        >
+          <h2>unSelected</h2>
+          {Object.keys(remainInventory).map(item => (
+            <div
+              key={item}
+              className={styles.inventoryItem}
+              draggable
+              onDragStart={e => onDragStart(e, item, 'unselected')}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'x')}
+            onDragOver={onDragOver}
+          >
+            <h2>x</h2>
+            {Object.keys(xInventory).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'x')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'open')}
+            onDragOver={onDragOver}
+          >
+            <h2>open</h2>
+            {Object.keys(open).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'open')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'close')}
+            onDragOver={onDragOver}
+          >
+            <h2>close</h2>
+            {Object.keys(close).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'close')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'high')}
+            onDragOver={onDragOver}
+          >
+            <h2>high</h2>
+            {Object.keys(high).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'high')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div
+            className={styles.inventorySection}
+            onDrop={e => onDrop(e, 'low')}
+            onDragOver={onDragOver}
+          >
+            <h2>low</h2>
+            {Object.keys(low).map(item => (
+              <div
+                key={item}
+                className={styles.inventoryItem}
+                draggable
+                onDragStart={e => onDragStart(e, item, 'low')}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  function customizeTooltip(arg: {
+    openValue: number;
+    closeValue: number;
+    highValue: number;
+    lowValue: number;
+  }) {
+    return {
+      text: `Open: ${arg.openValue}<br/>
+Close: ${arg.closeValue}<br/>
+High: ${arg.highValue}<br/>
+Low: ${arg.lowValue}<br/>`
+    };
+  }
+
   return (
-    <div>
-      <h1>CandleStick</h1>
-    </div>
+    <>
+      <input
+        type="file"
+        accept=".xlsx, .xls"
+        onChange={e =>
+          handleFileUpload(e, inventory, setRemainInventory, setExcel, [
+            setXInventory,
+            setOpen,
+            setClose,
+            setHigh,
+            setLow
+          ])
+        }
+        style={{
+          height: '100px',
+          border: '1px solid blue',
+          marginBlock: '20px'
+        }}
+      />
+      {Object.keys(inventory.current).length > 0 ? <InventoryBox /> : null}
+      <Chart id="chart" title="Candlestick Financial" dataSource={dataSource}>
+        <CommonSeriesSettings
+          argumentField={Object.keys(xInventory)[0]}
+          type="candlestick"
+        />
+        <Series
+          name={Object.keys(xInventory)[0]}
+          openValueField="o"
+          highValueField="h"
+          lowValueField="l"
+          closeValueField="c"
+        >
+          <Reduction color="red" />
+        </Series>
+
+        {/* format setting: 0/0/0000 */}
+        <ArgumentAxis workdaysOnly={true}>
+          <Label format="shortDate" />
+        </ArgumentAxis>
+
+        {/* y value interval, range setting(need to fix value) */}
+        <ValueAxis
+          tickInterval={1}
+          visualRange={{ startValue: -3000000, endValue: 5000000 }}
+        >
+          <Title text="Number View" />
+          <Label>
+            <Format precision={0} />
+          </Label>
+        </ValueAxis>
+
+        {/* location of chart property */}
+        <Legend itemTextPosition="left" />
+
+        <Export enabled={true} />
+        <Tooltip
+          enabled={true}
+          location="edge"
+          customizeTooltip={customizeTooltip}
+        />
+        <ZoomAndPan argumentAxis="both" valueAxis="both" />
+      </Chart>
+    </>
   );
 }
